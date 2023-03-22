a41f6a552b6d448b8c4030126c83d7692d8fa821
patricklohn <patricklohn3@gmail.com>
2023-03-21 21:26:01 -0300

    Teste 02

--- Principal.java
+++ Principal.java
@@ -1,8 +1,10 @@
 public class Principal {
     public static void main(String args[]) {
-        Trabalhador severino = new Trabalhador("sapato", 100);
-        Trabalhador raimundo = new Trabalhador("bota", 500);
+        Trabalhador severino = new Trabalhador("sapato", 200);
+        Trabalhador raimundo = new Trabalhador("bota", 200);
+        Trabalhador estefano = new Trabalhador("Casaco", 200);
         severino.start();
         raimundo.start();
+        estefano.start();
     }
 }
--- Trabalhador.java
+++ Trabalhador.java
@@ -1,18 +1,20 @@
 public class Trabalhador extends Thread {
-private String produto;
-private int tempo;
-public Trabalhador(String produto, int tempo) {
-this.produto = produto;
-this.tempo = tempo;
-}
-public void run() {
-for (int i = 0; i < 50; i++) {
-System.out.println(i + " " + produto);
-try {
-sleep((long)(Math.random() * tempo));
-}
-catch (InterruptedException e) {}
-}
-System.out.println("Terminei " + produto);
-}
+    private String produto;
+    private int tempo;
+
+    public Trabalhador(String produto, int tempo) {
+        this.produto = produto;
+        this.tempo = tempo;
+    }
+
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(i + " " + produto);
+            try {
+                sleep((long) (Math.random() * tempo));
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Terminei " + produto);
+    }
 }
